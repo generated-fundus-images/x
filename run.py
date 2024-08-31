31444dfed99e2b177d5a02272b3448525ce5d5b0
xlu <xlu103@126.com>
2024-09-01 01:44:32 +0800

    更改样式和图片类型

--- run.py
+++ run.py
@@ -2,10 +2,14 @@ import os
 import json
 
 folders = []
+print(1)
+print(os.listdir("D:/_code/x58/"))
 
 # 获取当前目录下的所有子文件夹
-for folder_name in os.listdir("./"):
+for folder_name in os.listdir("D:/_code/x58/"):
+    print(folder_name)
     if os.path.isdir(folder_name):
+        
         # 获取每个文件夹下的图片文件
         images = [f for f in os.listdir(folder_name) if f.endswith(".png")]
 
